8eef7bc41112b7ed11e2343830228278c9c930b7
Vesly <74131882+Veslydev@users.noreply.github.com>
2026-03-10 22:51:10 +0300

    awdawd

--- yolk/entrypoint.sh
+++ yolk/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd /home/container
 
+# CS:GO ships an old bundled bin/libgcc_s.so.1 that lacks GCC_7.0.0 symbols.
+# Remove it so the system lib32gcc-s1 version is used instead.
+rm -f /home/container/bin/libgcc_s.so.1
+
 # Replace Pelican {{VAR}} placeholders with shell ${VAR} equivalents, then eval-expand them
 MODIFIED_STARTUP=$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")
 
